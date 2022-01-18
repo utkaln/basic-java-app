bfa14d4f66ca073d9c775875bb401da924629630
Utkal Nayak <nayak.utkal@gmail.com>
2022-01-18 17:20:26 -0500

    debugging docker-compose

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -4,6 +4,6 @@ sudo service docker start
 sudo usermod -aG docker ec2-user
 
 # install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
