bf60d161019a73ba9bf0cab42eacb148cc6ca907
Utkal Nayak <nayak.utkal@gmail.com>
2022-01-18 00:21:18 -0500

    route table ip is string

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "aws_route_table" "utkal-rt" {
   route {
     // default for VPC is created implicitly
     // start with Internet Gateway
-    cidr_block = ["0.0.0.0/0"]
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.utkal-igw.id
   }
   tags = {
