986cb2f0f11261503702323b4eaf1aa4f15f6c3d
Utkal Nayak <nayak.utkal@gmail.com>
2022-01-18 16:38:10 -0500

    docker compose permission to 755

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -6,4 +6,6 @@ docker run -p 8080:80 nginx
 
 # install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod 666 /usr/local/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
+sudo docker-compose version
+
